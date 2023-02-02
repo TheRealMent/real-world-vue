85f70fb1dd9e7295fadb97a6f4378a4266c8540a
TheRealMent <therealmentweb@gmail.com>
2023-02-02 13:04:19 +0100

    Vue 3 Composition Finished

---
+++ src/views/EventListView.vue
@@ -0,0 +1,37 @@
+<script setup>
+import EventCard from '@/components/EventCard.vue';
+import EventService from '@/services/EventService.js';
+import { ref, onMounted } from 'vue';
+
+const events = ref(null)
+
+onMounted( async () => {
+ 
+  try {
+    const response =  await EventService.getEvents()
+    events.value = response.data
+    
+    
+  } catch (error) {
+    console.log(error)
+  }
+    
+  
+})
+
+</script>
+
+<template>
+  <div class="events" >
+    <EventCard  v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
+}
+</style>
